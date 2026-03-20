f804be4f0ed84e11c236bbdf905b0b16c1bc92f4
karinakhairull <khairullova.krina@gmail.com>
2026-03-20 16:39:31 +0300

    проект 02-echo обновленный (отправка команд из терминала)

--- 02-echo/main.c
+++ 02-echo/main.c
@@ -1,11 +1,34 @@
 ﻿#include <stdio.h>
 #include "pico/stdlib.h"
 
+#define DEVICE_NAME "my-pico-device"
+#define DEVICE_VERSION "v1.0.0"
+
+const uint LED_PIN = 25;
+
 int main() {
     stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 0);
 
     while (1) {
-        char symbol = getchar();
-        printf("received char: %c [ ASCII code: %d ]\n", symbol, symbol);
+        char symbol = getchar();//getchar получает символы с клавиатуры
+        printf("received char: %c [ ASCII code: %d ]\n", symbol, symbol);// выводит стандартное сообщение
+        switch(symbol){ //switch проверяет какой это символ
+        case 'e':
+            gpio_put(LED_PIN, 1);
+            printf("led enable done\n");
+            break;
+        case 'd':
+            gpio_put(LED_PIN, 0);
+            printf("led disable done\n");
+            break;
+        case 'v':
+            printf("Device: %s, Version: %s\n", DEVICE_NAME, DEVICE_VERSION);
+            break;
+default: // любая другая клавиша - ничего не делаем
+	break;}
+
     }
 }
